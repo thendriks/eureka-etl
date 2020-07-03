ae64711a19c055411847d3e86b0313f1897bb7fa
thendriks <t.hendriks@maastrichtuniversity.nl>
2020-07-03 12:29:45 +0200

    Created initial Spring template.

---
+++ src/main/java/nl/maastrichtuniversity/ids/eureka/etl/EurekaEtlApplication.java
@@ -0,0 +1,13 @@
+package nl.maastrichtuniversity.ids.eureka.etl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaEtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/maastrichtuniversity/ids/eureka/etl/EurekaEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.maastrichtuniversity.ids.eureka.etl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaEtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
